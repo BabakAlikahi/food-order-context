9d012f60f70923cd6149ba6d7f70139e709d0325
babakalikahi72 <babak.alikahi@gmail.com>
2024-08-21 21:01:16 +0430

    check can get data or not

---
+++ src/components/Meals.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+
+function Meals() {
+  async function fetchMeals() {
+    // dev Mood
+    // const response = await fetch("http://localhost:3000/meals");
+    
+    // Product Mood
+    const response = await fetch("http://localhost:3000/meals");
+    if (!response.ok) {
+    }
+    const resData = await response.json();
+    console.log(resData);
+}
+  useEffect(() => {
+    fetchMeals();
+    console.log('hiii');
+  }, []);
+
+  return (
+    <ul id="meals">
+      <li></li>
+    </ul>
+  );
+}
+
+export default Meals;
