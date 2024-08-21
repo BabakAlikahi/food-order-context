15bc95065941ef7b255c451ef1f80db32e815f92
babakalikahi72 <babak.alikahi@gmail.com>
2024-08-21 22:06:24 +0430

    Update Meals.jsx

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -2,21 +2,14 @@ import React, { useEffect } from "react";
 
 function Meals() {
   async function fetchMeals() {
-    // dev Mood
-    // const response = await fetch("http://localhost:3000/meals");
-    
-    // Product Mood
-    const response = await fetch("https://food-order-context-back.vercel.app/meals",{
-      mode: 'no-cors'
-    });
+    const response = await fetch("http://localhost:3000/meals");
+
     if (!response.ok) {
     }
     const resData = await response.json();
-    console.log(resData);
-}
+  }
   useEffect(() => {
     fetchMeals();
-    console.log('hiii');
   }, []);
 
   return (
